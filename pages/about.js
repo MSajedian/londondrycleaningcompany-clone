1917cb15afa41d35a07827f0a0bdaa01be1e29b6
Mohammadamin Sajedian <mohammadsajedian@gmail.com>
2022-05-15 17:18:26 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.css'
+
+const About = () => {
+    return (
+      <div>
+        <h1 className={styles.title}>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      </div>
+    );
+  }
+   
+  export default About;
