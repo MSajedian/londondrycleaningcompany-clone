cf5d82964d6f0f808ad53f0662971148efed5c7d
MSajedian <mohammadsajedian@gmail.com>
2022-05-22 01:37:51 +0100

    updated

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
               <Card.Img variant="top" className="services-card-img" src="/service_home2-min.png" alt="Laundry" />
               <Card.Body>
                 <Card.Title className="text-center">Laundry</Card.Title>
-                <Card.Text className="text-center">
+                <Card.Text className="text-center text-secondary">
                   We only use a special detergent exclusively developed for different types of fabric from Cotton shirts to silk Bedsheets.
                 </Card.Text>
               </Card.Body>
@@ -97,7 +97,7 @@ export default function Home() {
           <br />
           The solvent used is typically Perchloroethylene
         </h5>
-        <Row xs={1} sm={2} className="g-4">
+        <Row xs={1} sm={2} className="g-4 mt-2">
           <Tab.Container id="tabs" defaultActiveKey="Exceptional service">
             <Col>
               <Tab.Content>
@@ -133,13 +133,13 @@ export default function Home() {
             <Col>
               <Nav variant="pills" className="flex-row">
                 <Nav.Item>
-                  <Nav.Link eventKey="Exceptional service">Exceptional service</Nav.Link>
+                  <Nav.Link className="services-love-card-nav-link" eventKey="Exceptional service">Exceptional service</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="Fast delivery">Fast delivery</Nav.Link>
+                  <Nav.Link className="services-love-card-nav-link" eventKey="Fast delivery">Fast delivery</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="Spotless finish">Spotless finish</Nav.Link>
+                  <Nav.Link className="services-love-card-nav-link" eventKey="Spotless finish">Spotless finish</Nav.Link>
                 </Nav.Item>
               </Nav>
               <hr />
@@ -184,7 +184,7 @@ export default function Home() {
 
             <Row xs={1} sm={2} md={3} lg={4} className="p-0 m-0">
               <Col >
-                <Badge pill bg="primary">01</Badge>
+                <div className="d-flex justify-content-end"><Badge pill bg="primary">01</Badge></div>
                 <Image src="/tracking-min-2.png"
                   alt="Choose Any Time For Delivery"
                   width={64}
@@ -194,7 +194,7 @@ export default function Home() {
                 Choose Any Time For Delivery
               </Col>
               <Col >
-                <Badge pill bg="primary">02</Badge>
+                <div className="d-flex justify-content-end"><Badge pill bg="primary">02</Badge></div>
                 <Image src="/laundry-min.png"
                   alt="Choose Any Time For Delivery"
                   width={64}
@@ -204,7 +204,7 @@ export default function Home() {
                 We Can Wash and Dry Dirty Laundry
               </Col>
               <Col >
-                <Badge pill bg="primary">03</Badge>
+                <div className="d-flex justify-content-end"><Badge pill bg="primary">03</Badge></div>
                 <Image src="/shirt-min.png"
                   alt="Choose Any Time For Delivery"
                   width={64}
@@ -214,7 +214,7 @@ export default function Home() {
                 We Return Your Clean Laundry
               </Col>
               <Col >
-                <Badge pill bg="primary">04</Badge>
+                <div className="d-flex justify-content-end"><Badge pill bg="primary">04</Badge></div>
                 <Image src="/tea-min.png"
                   alt="Choose Any Time For Delivery"
                   width={64}
@@ -327,10 +327,10 @@ export default function Home() {
 
         <svg
           style={{
-            transform: "scale(-1,-1)",
-            backgroundColor: "transparent"
+            transform: "scale(-1,-1) translate(0, -1px)",
+            backgroundColor: "transparent",
           }}
-          className="mt-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 283.5 27.8" preserveAspectRatio="xMidYMax slice" fill="#f6f6f5" >
+          className="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 283.5 27.8" preserveAspectRatio="xMidYMax slice" fill="#f6f6f5" >
           <path className="" d="M0 0v6.7c1.9-.8 4.7-1.4 8.5-1 9.5 1.1 11.1 6 11.1 6s2.1-.7 4.3-.2c2.1.5 2.8 2.6 2.8 2.6s.2-.5 1.4-.7c1.2-.2 1.7.2 1.7.2s0-2.1 1.9-2.8c1.9-.7 3.6.7 3.6.7s.7-2.9 3.1-4.1 4.7 0 4.7 0 1.2-.5 2.4 0 1.7 1.4 1.7 1.4h1.4c.7 0 1.2.7 1.2.7s.8-1.8 4-2.2c3.5-.4 5.3 2.4 6.2 4.4.4-.4 1-.7 1.8-.9 2.8-.7 4 .7 4 .7s1.7-5 11.1-6c9.5-1.1 12.3 3.9 12.3 3.9s1.2-4.8 5.7-5.7c4.5-.9 6.8 1.8 6.8 1.8s.6-.6 1.5-.9c.9-.2 1.9-.2 1.9-.2s5.2-6.4 12.6-3.3c7.3 3.1 4.7 9 4.7 9s1.9-.9 4 0 2.8 2.4 2.8 2.4 1.9-1.2 4.5-1.2 4.3 1.2 4.3 1.2.2-1 1.4-1.7 2.1-.7 2.1-.7-.5-3.1 2.1-5.5 5.7-1.4 5.7-1.4 1.5-2.3 4.2-1.1c2.7 1.2 1.7 5.2 1.7 5.2s.3-.1 1.3.5c.5.4.8.8.9 1.1.5-1.4 2.4-5.8 8.4-4 7.1 2.1 3.5 8.9 3.5 8.9s.8-.4 2 0 1.1 1.1 1.1 1.1 1.1-1.1 2.3-1.1 2.1.5 2.1.5 1.9-3.6 6.2-1.2 1.9 6.4 1.9 6.4 2.6-2.4 7.4 0c3.4 1.7 3.9 4.9 3.9 4.9s3.3-6.9 10.4-7.9 11.5 2.6 11.5 2.6.8 0 1.2.2c.4.2.9.9.9.9s4.4-3.1 8.3.2c1.9 1.7 1.5 5 1.5 5s.3-1.1 1.6-1.4c1.3-.3 2.3.2 2.3.2s-.1-1.2.5-1.9 1.9-.9 1.9-.9-4.7-9.3 4.4-13.4c5.6-2.5 9.2.9 9.2.9s5-6.2 15.9-6.2 16.1 8.1 16.1 8.1.7-.2 1.6-.4V0H0z"></path>
         </svg>
         <div className="text-center"
